85a80fc4b4d4df43c820906b82d9ad2140f4c490
petter15 <petter15@hotmail.com>
2018-01-24 13:43:12 +0100

    Initial commit

---
+++ pca10040/s132/armgcc/_build/nrf52832_xxaa_ble_hids.c.d
@@ -0,0 +1,203 @@
+_build/nrf52832_xxaa_ble_hids.c.o: \
+ ../../../../../../components/ble/ble_services/ble_hids/ble_hids.c \
+ ../../../../../../components/libraries/util/sdk_common.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\stdint.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\_default_types.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\features.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\_newlib_version.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_intsup.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_stdint.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdbool.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\string.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\_ansi.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\newlib.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\config.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\ieeefp.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\reent.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\_ansi.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_types.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\_types.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\lock.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\cdefs.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\string.h \
+ ../config/sdk_config.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_os.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_macros.h \
+ ../../../../../../components/ble/ble_services/ble_hids/ble_hids.h \
+ ../../../../../../components/softdevice/s132/headers/ble.h \
+ ../../../../../../components/softdevice/s132/headers/ble_ranges.h \
+ ../../../../../../components/softdevice/s132/headers/ble_types.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gap.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../../components/softdevice/s132/headers/ble_l2cap.h \
+ ../../../../../../components/softdevice/s132/headers/ble_err.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gatt.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gattc.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gatts.h \
+ ../../../../../../components/ble/common/ble_srv_common.h \
+ ../../../../../../components/softdevice/s132/headers/ble_types.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gap.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gatt.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\stdio.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdarg.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\types.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_pthreadtypes.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\types.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\stdio.h \
+ ../../../../../../components/libraries/util/app_error_weak.h
+
+../../../../../../components/libraries/util/sdk_common.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\stdint.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\_default_types.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\features.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\_newlib_version.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_intsup.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_stdint.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdbool.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\string.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\_ansi.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\newlib.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\config.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\ieeefp.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\reent.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\_ansi.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_types.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\_types.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\lock.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\cdefs.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\string.h:
+
+../config/sdk_config.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/sdk_os.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../components/toolchain/system_nrf52.h:
+
+../../../../../../components/device/nrf52_bitfields.h:
+
+../../../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../../../components/device/nrf52_name_change.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/sdk_macros.h:
+
+../../../../../../components/ble/ble_services/ble_hids/ble_hids.h:
+
+../../../../../../components/softdevice/s132/headers/ble.h:
+
+../../../../../../components/softdevice/s132/headers/ble_ranges.h:
+
+../../../../../../components/softdevice/s132/headers/ble_types.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gap.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_svc.h:
+
+../../../../../../components/softdevice/s132/headers/ble_l2cap.h:
+
+../../../../../../components/softdevice/s132/headers/ble_err.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gatt.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gattc.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gatts.h:
+
+../../../../../../components/ble/common/ble_srv_common.h:
+
+../../../../../../components/softdevice/s132/headers/ble_types.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gap.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gatt.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\stdio.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdarg.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\types.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_pthreadtypes.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\types.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\stdio.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
---
+++ pca10040/s132/armgcc/_build/nrf52832_xxaa_sensorsim.c.d
@@ -0,0 +1,29 @@
+_build/nrf52832_xxaa_sensorsim.c.o: \
+ ../../../../../../components/libraries/sensorsim/sensorsim.c \
+ ../../../../../../components/libraries/sensorsim/sensorsim.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\stdint.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\_default_types.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\features.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\_newlib_version.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_intsup.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_stdint.h \
+ c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdbool.h
+
+../../../../../../components/libraries/sensorsim/sensorsim.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\stdint.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\machine\_default_types.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\features.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\_newlib_version.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_intsup.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\arm-none-eabi\include\sys\_stdint.h:
+
+c:\gnu_tools_arm_embedded\6_2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdbool.h:
